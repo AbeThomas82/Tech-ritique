9dec8f1597e364fdfa90d0b7088a0b97ecd1f246
Sherwin Abraham <sherwinabraham@gmail.com>
2023-09-15 20:38:07 -0500

    Working on login some more.

---
+++ public/js/login.js
@@ -0,0 +1,18 @@
+
+async function handleLogIn(e) {
+    e.preventDefault()
+    const username = document.querySelector('#loginUsername').value.trim()
+    const password = document.querySelector('#loginPassword').value.trim()
+    console.log(username,password)
+    const response = await fetch('/api/users/login',{
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({username, password})
+    })
+    if (response.ok) {
+        document.location.replace('/dashboard')
+    }else {
+        alert("Failed to log in.")
+    }
+}
+document.querySelector('.login').addEventListener('submit',handleLogIn)
