cf00f8e4e6ecb52a6630fd6a479a61761e3b9396
Sherwin Abraham <sherwinabraham@gmail.com>
2023-09-16 23:50:57 -0500

    Tinkering still.

--- public/js/login.js
+++ public/js/login.js
@@ -15,4 +15,30 @@ async function handleLogIn(e) {
         alert("Failed to log in.")
     }
 }
-document.querySelector('.login').addEventListener('submit',handleLogIn)
+
+async function handleRegister(e) {
+    e.preventDefault();
+  
+    const username = document.querySelector('#createUsername').value.trim();    
+    const password = document.querySelector('#createPassword').value.trim();    
+     
+    if (username && password) {
+      const response = await fetch('/api/users/', {
+        method: 'post',
+        body: JSON.stringify({
+          username,          
+          password
+        }),
+        headers: { 'content-type': 'application/json' } 
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to register.');
+      }
+    }
+}
+
+document.querySelector('.login').addEventListener('submit', handleLogIn);
+document.querySelector('.signup').addEventListener('submit', handleRegister);
